a52fb0009aa8c4d502cd80c4a7e60d2d324f12bd
Larissa Guimaraes <111644936+Lellis17@users.noreply.github.com>
2023-04-12 22:26:34 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        ContaCorrente cc = new ContaCorrente();
-        cc.depositar(1200.20);
-        cc.sacar(300);
+        Funcoes f = new Funcoes();
+        Menu m = new Menu();
 
-        ContaPoupanca cp = new ContaPoupanca();
-        cp.depositar(500.50);
-        cp.sacar(25);
+        Scanner ler = new Scanner(System.in);
 
+        System.out.println("Informe o Fabricante \n 1- Fabricante A \n 2- Fabricante B");
+        int opcao = ler.nextInt();
 
-        GeradorExtratos gerador = new GeradorExtratos();
-        gerador.geradorConta(cc);
-        gerador.geradorConta(cp);
+        if (opcao == 1) {
+            System.out.println("Fabricante A foi selecionado");
+            f.FabricanteA();
 
+        } else if (opcao == 2) {
+            System.out.println("Fabricante B foi selecionado");
+            f.FabricanteB();
+        }
+
+        else
+            System.out.println("OpÃ§Ã£o invalida");
     }
+
+
 }
